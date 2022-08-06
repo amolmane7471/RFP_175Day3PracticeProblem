f5e9f988a287765c814a5f0926351c24b802e924
amolmane7471 <maneamol3233@gmail.com>
2022-08-06 09:42:00 +0530

    [Amol]:added a UC3 for part time employee & wage

--- src/com/bridgelabz/empwage/EmpWage.java
+++ src/com/bridgelabz/empwage/EmpWage.java
@@ -3,18 +3,27 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 	System.out.println("Welcome To Employee Wage Computation Problem");
-	int IS_FULLTIME=1;
-	int EMP_RATE_PER_HOUR=20;
+	int IS_PARTTIME = 2;
+	int IS_FULLTIME = 1;
+	int EMP_RATE_PER_HOUR = 20;
 	int empHrs;
 	int empWage;
-	double empcheck=Math.floor(Math.random()*10)%2;
+	double empcheck = Math.floor(Math.random()*10)%3;
 	/*
 	 * UC1 : Employee Attendance
 	 */
-	if(empcheck==IS_FULLTIME)
+	if(empcheck == IS_FULLTIME)
 	{
-	empHrs=8;
-	System.out.println("Emp is Present");
+	empHrs = 8;
+	System.out.println("Emp is Full Time Present");
+	}
+	/*
+	 * UC3 : Part Time Employee and wage
+	 */
+	else if(empcheck == IS_PARTTIME)
+	{
+	empHrs = 4;
+	System.out.println("Emp is Part Time Present");
 	}
 	else 
 	{
